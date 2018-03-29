547ff185e82854e2759a0d3a35ff133f2597b0af
Maksym Popenko <popenkomaksim@gmail.com>
2018-03-29 11:56:47 +0300

    Update slides

--- presentation/slides/Dokku/TerminalExample.js
+++ presentation/slides/Dokku/TerminalExample.js
@@ -34,30 +34,29 @@ export default (
       showFirstEntry
       title="1. maksym@popenko: ~(zsh)"
       output={[
-        <div>create-react-app just-few-dependencies</div>,
         <Typist cursor={{ hideWhenDone: true, hideWhenDoneDelay: 100 }}>
-          yarn build
+          $ git push dokku master
         </Typist>,
         <div>
-          <div> </div>
-          <div>Creating an optimized production build...</div>
-          <div style={{ color: colors.green }}>
-            Compiled successfully.
-          </div>
-          <div> </div>
-          <div>File sizes after gzip:</div>
-          <div>
-            47.09 KB (151 KB) build/static/js/<span
-            style={{ color: colors.green }}
-          >
-                    main.8b0e51a6.js
-                  </span>
-          </div>
-          <div>
-            289 B build/static/css/<span style={{ color: colors.green }}>
-                    main.9a0fe4f1.css
-                  </span>
-          </div>
+          > git pushes code as is into Dokku repo
+        </div>,
+        <div>
+          > Create an optimized production build...
+        </div>,
+        <div>
+          > Install and cache dependencies (npm node node_modules etc.)
+        </div>,
+        <div>
+          > Set env variables inside Docker container
+        </div>,
+        <div>
+          > do NGINX hot-port switch between old and new container
+        </div>,
+        <div>
+          > Drain connection from old container
+        </div>,
+        <div>
+          Deployed https://tickets.example.com
         </div>
       ]}
     />
