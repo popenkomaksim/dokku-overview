a22c2ec7cf3cb5479001b3049d207790c85a4465
Maksym Popenko <popenkomaksim@gmail.com>
2018-04-05 10:16:36 +0300

    Reorder slides

--- presentation/index.js
+++ presentation/index.js
@@ -43,9 +43,9 @@ const slidesImports = [
   import("./components/DataArtSlide"),
 
   import("./slides/Intro/WhatIsDeploy"),
-  import("./slides/Intro/ManualDeployment"),
   import("./slides/Intro/MyDeploymentPath"),
   import("./slides/Intro/TheStoryOfDeploy"),
+  import("./slides/Intro/ManualDeploymentMeme"),
   import("./slides/Intro/Joke"),
 
   import("./slides/Quotes/ManagingServers"),
